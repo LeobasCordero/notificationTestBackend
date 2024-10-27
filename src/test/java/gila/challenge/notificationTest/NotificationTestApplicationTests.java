47948ebe6ec6c556659c67046e67f04fab76d9bb
LeobasCordero <leocorderoguzman@gmail.com>
2024-10-27 08:01:38 -0600

    first commit

---
+++ src/test/java/gila/challenge/notificationTest/NotificationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package gila.challenge.notificationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
