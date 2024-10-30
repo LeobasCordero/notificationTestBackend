b71d3139e83dd6e69c622de4ccc444182e419b47
LeobasCordero <leocorderoguzman@gmail.com>
2024-10-29 23:57:13 -0600

    fix for datetime

--- src/main/java/gila/challenge/notificationTest/service/MessageService.java
+++ src/main/java/gila/challenge/notificationTest/service/MessageService.java
@@ -13,7 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -43,7 +45,7 @@ public class MessageService {
         Category category = categoryService.getCategoryById(notificationDto.getCategoryId());
         Channel channel = channelService.getChannelByName(channelName);
         var message = MessageMapper.NotificationDTOtoMessage(user, category, channel, notificationDto);
-        message.setSentAt(LocalDateTime.now());
+        message.setSentAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
 
         logger.info("MessageService.saveMessage: storing message for userID: {}", user.getUserId());
         return messageRepository.save(message);
